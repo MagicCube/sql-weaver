0410b2820fda12edab194924177d1b341c1e3382
Henry Li <henry1943@163.com>
2023-04-03 15:28:29 +0800

    feat: add executeQuery()

--- src/ui/stores/query.ts
+++ src/ui/stores/query.ts
@@ -1,9 +1,20 @@
 export class QueryStore {
   query = 'SELECT * FROM Customers';
+  results: unknown[] = [];
 
   setQuery(query: string) {
     this.query = query;
   }
 
-  executeQuery() {}
+  async executeQuery() {
+    const res = await fetch('/api/sqlite/northwind/query', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query: this.query }),
+    });
+    const results = await res.json();
+    this.results = results;
+  }
 }
