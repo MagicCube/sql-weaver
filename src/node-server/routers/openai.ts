3c006350cd98415498c5947a98df35f52a1a322f
Henry Li <henry1943@163.com>
2023-04-03 18:28:23 +0800

    feat: implement ask

--- src/node-server/routers/openai.ts
+++ src/node-server/routers/openai.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import type { CreateChatCompletionRequest } from 'openai';
 import { Configuration, OpenAIApi } from 'openai';
 
 export const router: Router = Router();
@@ -16,3 +17,46 @@ const openai = new OpenAIApi(
     apiKey: process.env.OPENAI_API_KEY,
   })
 );
+
+router.post('/ask', async (req, res) => {
+  const completionReq: CreateChatCompletionRequest = {
+    model: 'gpt-3.5-turbo',
+    messages: req.body.messages,
+  };
+  if (req.query.stream === 'true') {
+    const partialCompletionRes = await openai.createChatCompletion(
+      {
+        ...completionReq,
+        stream: true,
+      },
+      {
+        responseType: 'stream',
+      }
+    );
+    // @ts-ignore
+    partialCompletionRes.data.on('data', (data: ReadableStream) => {
+      const chunks = data
+        .toString()
+        .trim()
+        .split('\n\n')
+        .map((l) => l.trim().split('data: ')[1]);
+      chunks.forEach((chunk) => {
+        if (chunk === '[DONE]') {
+          res.end();
+        } else {
+          const json = JSON.parse(chunk);
+          if (json.choices[0].delta.content) {
+            const content = json.choices[0].delta.content;
+            res.write(`data: ${content}\n\n`);
+          }
+        }
+      });
+    });
+    res.type('text/event-stream');
+  } else {
+    const completionRes = await openai.createChatCompletion({
+      ...completionReq,
+    });
+    res.end(completionRes.data.choices[0].message?.content);
+  }
+});
