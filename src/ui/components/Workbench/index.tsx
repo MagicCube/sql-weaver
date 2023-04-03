3903373e97442a97642d542358ee363d2bff2ae6
Henry Li <henry1943@163.com>
2023-04-03 14:14:46 +0800

    feat: enhance introduction

--- src/ui/components/Workbench/index.tsx
+++ src/ui/components/Workbench/index.tsx
@@ -16,11 +16,12 @@ export function Workbench({ className }: WorkbenchProps) {
     <div className={cn(styles.container, className)}>
       <aside className={styles.sideBar}>
         <Tooltip
+          className={styles.introduction}
           content={
-            <>
+            <div>
               <h3 style={{ marginTop: 0, marginBottom: 4 }}>欢迎使用 SQLWeaver</h3>
               <div>SQLWeaver 是一款 AI 驱动的 SQL 查询工具，旨在帮助您通过自然语言对话完成数据查询任务。</div>
-            </>
+            </div>
           }
           position="right"
         >
