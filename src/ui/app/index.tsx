b3a0c7cc3e2d7f386e3d94714818bb615190b702
Henry Li <henry1943@163.com>
2023-04-03 16:22:38 +0800

    feat: change locale to EN-US

--- src/ui/app/index.tsx
+++ src/ui/app/index.tsx
@@ -1,11 +1,16 @@
+import { ConfigProvider } from '@arco-design/web-react';
+import enUS from '@arco-design/web-react/es/locale/en-US';
+
 import { Workbench } from '../components/Workbench';
 
 import styles from './index.module.less';
 
 export function App() {
   return (
-    <div className={styles.container}>
-      <Workbench />
-    </div>
+    <ConfigProvider locale={enUS}>
+      <div className={styles.container}>
+        <Workbench />
+      </div>
+    </ConfigProvider>
   );
 }
--- src/ui/components/MainMenu/index.tsx
+++ src/ui/components/MainMenu/index.tsx
@@ -19,21 +19,21 @@ export function MainMenu({ className }: MainMenuProps) {
   return (
     <menu className={cn(styles.container, className)}>
       <li>
-        <Tooltip content="主页" position="right">
+        <Tooltip content="Home" position="right">
           <a className={styles.item}>
             <HomeOutlined />
           </a>
         </Tooltip>
       </li>
       <li>
-        <Tooltip content="智能辅助查询" position="right">
+        <Tooltip content="Query Weaver" position="right">
           <a className={cn(styles.active, styles.item)}>
             <TableOutlined />
           </a>
         </Tooltip>
       </li>
       <li>
-        <Tooltip content="数据可视化" position="right">
+        <Tooltip content="Vision Weaver" position="right">
           <a className={styles.item}>
             <PieChartOutlined />
           </a>
@@ -41,14 +41,14 @@ export function MainMenu({ className }: MainMenuProps) {
       </li>
       <li className={styles.spring}></li>
       <li>
-        <Tooltip content="系统设置" position="right">
+        <Tooltip content="Settings" position="right">
           <a className={styles.item}>
             <SettingOutlined />
           </a>
         </Tooltip>
       </li>
       <li>
-        <Tooltip content="Indoc 帮助中心" position="right">
+        <Tooltip content="Indoc Help Center" position="right">
           <a className={styles.item}>
             <QuestionCircleOutlined />
           </a>
